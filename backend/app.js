b2da8fa6d391dcdec2a5ef1abf040783756dda01
nomankhan7776 <nknomankhan30@gmail.com>
2024-10-13 20:09:16 +0500

    setup mongodb/created user,products schemas & models/created user routes/created controllers for user login and register

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudnary.js";
+import userRouter from "./routes/userRoute.js";
+//app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
+//middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+//api endpoint
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("api working");
---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,70 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createToken from "../utils/generateToken.js";
+
+//route for login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!validator.isEmail(email))
+    return res
+      .status(404)
+      .json({ success: false, message: "email is INVALID" });
+  const existUser = await userModel.findOne({ email });
+  if (!existUser) {
+    return res
+      .status(404)
+      .json({ success: false, message: "invalid email and password" });
+  }
+  const isMatch = await bcrypt.compare(password, existUser.password);
+  if (isMatch) {
+    const token = createToken(existUser._id);
+    res.status(200).json({ success: true, token });
+  } else {
+    res
+      .status(404)
+      .json({ success: false, message: "invalid email and password" });
+  }
+};
+
+//route for register user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existUser = await userModel.findOne({ email });
+    if (existUser)
+      return res
+        .status(409)
+        .json({ success: false, message: "user already exist" });
+    if (!validator.isEmail(email))
+      return res.json({ success: false, message: "Enter a valid Email" });
+
+    if (password.length < 8)
+      return res.json({
+        success: false,
+        message: "Please Enter a strong Password",
+      });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.send({ success: false, message: error.message });
+  }
+};
+
+//route for adminlogin
+const adminLogin = async (req, res) => {
+  res.status(200).json({ msg: "admin api working" });
+};
+
+export { loginUser, registerUser, adminLogin };
