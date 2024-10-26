190ba4e9742d731e1671c1c151bf312b9be05de4
nomankhan7776 <nknomankhan30@gmail.com>
2024-10-27 03:09:50 +0500

    adding cart controller/order cantroller/order model,route/

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ import connectCloudinary from "./config/cloudnary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/userCartRoute.js";
+import orderRouter from "./routes/orderRout.js";
+import { stripeWebhook } from "./controllers/orderController.js";
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -17,10 +19,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.post("/api/order/webhook", express.raw({ type: "application/json" }), stripeWebhook);
+
+// Content Security Policy middleware
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' https://js.stripe.com 'sha256-/5Guo2nzv5n/w6ukZpOBZOtTJBJPSkJ6mhHpnBgm3Ls='; style-src 'self' 'unsafe-inline';"
+  );
+  next();
+});
+
 //api endpoint
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order/", orderRouter);
 app.get("/", (req, res) => {
   res.send("api working");
 });
